62bd14fc2433456af7c1cc97c236c3385064634d
ReikoNyann <97742630+ReikoNyann@users.noreply.github.com>
2022-03-01 19:53:29 +0800

    Initial commit

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/login.css">
+    <header>
+        <title>Welcome to Mu2WIL</title>
+    </header>
+
+    </head>
+
+<body>
+    <main>
+        <div class="navbar">
+         
+            <nav>
+                <ul>
+                    <li><a href="#"><img src="img/Murdoch_University_extended_logo.png" alt="logo" class="logo"></a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="loginform">
+            <h2>MU2WIL LOGIN</h2>
+
+            <form action="#" method="post">
+            <label>Email: <input type="email" name="name"></label><br>
+            <label>Password: <input type="password" name="pword"></label><br>
+            <input type="submit">
+        </form> 
+        </div>
+    </main>
+        <footer>
+            <p>Copyright 2022, Team Yuen Yuen</p>
+            <a href="#">Contact Us</a>
+        </footer>
+    </body>
+</html>
