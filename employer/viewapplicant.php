34e31903fb067a22ba4c5e9257aeec852d8bccee
shiyuanhang0913 <shiyuanhang0913@163.com>
2022-03-23 22:41:43 +0800

    employer view application

--- employer/viewapplicant.php
+++ employer/viewapplicant.php
@@ -6,7 +6,11 @@
         //declare login userid 
         $uid = $_SESSION['uid'];
         //get data from table
-        $sql = "SELECT * FROM application WHERE CompanyID = $uid";
+        // $sql = "SELECT * FROM application WHERE CompanyID = $uid";
+        $sql = "SELECT * FROM ((application INNER JOIN student ON application.StudentID = student.StudentID) INNER JOIN job ON application.JobID = job.JobID)";
+        // $sql = "SELECT * FROM application
+        //         INNER JOIN job ON application.JobID = job.JobID INNER JOIN company ON job.CompanyID = company.CompanyID WHERE CompanyID = $uid";
+//job.JobTitle, job.JobPostDate, job.JobCloseDate, job.RoleResponsibility, job.JobCriteria, job.JobLocation, job.JobSalary, job.JobDuration
         //connect DB and sql query
         $result = mysqli_query($conn,$sql);
         //fetch results 
@@ -22,7 +26,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="../css/signup.css">
+    <link rel="stylesheet" href="../css/employer/viewapplicant.css">
     <header>
         <title>Mu2WIL View Application Page</title>
     </header>
@@ -31,32 +35,68 @@
 
 <body>
     <main>
+        <div class="logo">
+            <a href="dashboard.php"><img width="250px" src="../img/Murdoch_University_extended_logo.png" alt="logo"></a>
+        </div>
         <div class="navbar">
-            <nav>
-                <ul>
-                    <li><a href="../index.php"><img src="../img/Murdoch_University_extended_logo.png" alt="logo" class="logo"></a>
-                    </li>
-                </ul>
-            </nav>
+            <a href="../jobopportunities.php">Job Opportunities</a>
+            <a href="../contact.php">Contact Us</a>
+            <div class="dropdown">
+                <button class="droplist"><?php echo $row['PersonInCharge']; ?>
+                    <i class="togglelist"></i>
+                </button>
+                <div class="contentlist">
+                    <a href="profile.php">View Profile</a>
+                    <a href="addNewJob.php">Add New Job</a>
+                    <a href="viewapplicant.php">View Applicants</a>
+                    <a href="../logout.php">Logout</a>
+                </div>
+            </div>
         </div>
 
-        <!-- <div class="applicationform">
-            <h2>Application Details</h2>
-            <form action="" method="post">
-                <button name="submit" class="btn">View Application</button>
-            </form>
-        </div> -->
+        <!-- <table>
+            <tr>
+                <th>Apply Date</th>
+                <th>Job Type</th>
+                <th>Job Title</th>
+                <th>Job Location</th>
+                <th>Student Name</th>
+                <th>Student Contact tNumber</th>
+            </tr>
+            <tr>
+                <td><?php echo $row['ApplyDate']; ?></td>
+                <td><?php echo $row['JobType']; ?></td>
+                <td><?php echo $row['JobTitle']; ?></td>
+                <td><?php echo $row['JobLocation']; ?></td>
+                <td><?php echo $row['StudentName']; ?></td>
+                <td><?php echo $row['StudentContactNumber']; ?></td>
+            </tr>
+        </table> -->
 
-        <h4>Application ID: </h4>
-        <?php echo $row['ApplicationID']; ?><br />
-        <h4>Apply Date: </h4>
-        <?php echo $row['ApplyDate']; ?><br />
-        <h4>Apply Status: </h4>
-        <?php echo $row['ApplyStatus']; ?><br />
-        <h4>Job ID: </h4>
-        <?php echo $row['JobID']; ?><br />
-        <h4>Student Apply ID: </h4>
-        <?php echo $row['StudentID']; ?><br />
+        <table>
+	        <tr>
+		        <th>Apply Date</th>
+		        <th>Job Type</th>
+		        <th>Job Title</th>
+                <th>Job Location</th>
+                <th>Student Name</th>
+                <th>Student Contact Number</th>
+	        </tr>
+	        <?php
+            $sql = "SELECT * FROM ((application INNER JOIN student ON application.StudentID = student.StudentID) INNER JOIN job ON application.JobID = job.JobID)";
+            $r = mysqli_query($conn,$sql);
+	        while($row=mysqli_fetch_assoc($r)){
+            ?>
+	        <tr>
+		        <td><?php echo $row["ApplyDate"]; ?></td>
+		        <td><?php echo $row["JobType"]; ?></td>
+		        <td><?php echo $row["JobTitle"]; ?></td>
+                <td><?php echo $row["JobLocation"]; ?></td>
+		        <td><?php echo $row["StudentName"]; ?></td>
+		        <td><?php echo $row["StudentContactNumber"]; ?></td>
+	        </tr>
+	        <?php } ?>
+        </table>
 
     </main>
     <footer>
